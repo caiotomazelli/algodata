da1ed6ce7a4e8cd85898bc72cbb44e4cf402b633
caiotomazelli <caiotomazelli@gmail.com>
2021-10-03 10:56:16 -0300

    Increases recursion depth and times test execution.

--- sorting/sorting_test.py
+++ sorting/sorting_test.py
@@ -2,13 +2,18 @@ import unittest
 from random import randint
 from sorting import Sorting
 import copy
+import sys
+import time
+
+# increase recursion limit to allow for bigger test cases
+sys.setrecursionlimit(int(1e5))
 
 _CASE_LIST = [[],
               [1],
               [1,2,3],
               [9,8,7,6,5,4,3,2,1],
-              [1 for _ in range(int(1e2))],
-              [randint(-1e2, 1e2) for _ in range(int(1e2))]
+              [1 for _ in range(int(1e4))],
+              [randint(-1e4, 1e4) for _ in range(int(1e4))]
              ]
 
 _IMPLEMENTATION_LIST = [Sorting.qsort, Sorting.msort, Sorting.ssort]
@@ -22,10 +27,13 @@ class SortingTest(unittest.TestCase):
         for implementation in _IMPLEMENTATION_LIST:
             # clean-up test cases, as in-place implementations may have modified them
             self.setUp()
+            start = time.time()
             for case in self.cases:
                 with self.subTest(f'{implementation.__name__} sorting array of size {len(case)}'):
                     sorted_case = sorted(case)
                     self.assertListEqual(implementation(case), sorted_case)
+            end = time.time()
+            print(f'\n{implementation.__name__} finished cases in {(end-start):.1f} seconds.')
 
 if __name__ == '__main__':
     unittest.main(verbosity=2) 
